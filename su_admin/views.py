13072757e57efb1897f9c728a8d4310fd0a89874
Junaid mugloo <33572920+junaidmugloo@users.noreply.github.com>
2024-05-28 14:58:28 +0530

    s

---
+++ su_admin/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client['fake_review']
+# Create your views here.
+def index(res):
+    return render(res,'index.html');
